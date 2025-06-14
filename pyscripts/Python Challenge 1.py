22abbdc4444d4ca16ae6001a0e961ee41283aa37
james <jblkstne@gmail.com>
2025-06-14 17:06:13 -0400

    Python Challenge 1

---
+++ pyscripts/Python Challenge 1.py
@@ -0,0 +1,20 @@
+car_1 = {
+    "Make" : "Subaru",
+    "Model": "22B STI",
+    "Year" : 1998,
+    "Price" : None,
+    "Owned" : False,
+    "Colors" : ["Blue", "White", "Red"]
+}
+
+
+car_2 = {
+    "Make" : "Lucid",
+    "Model" : "Air",
+    "Year" : 2024,
+    "Price" : float(68000),
+    "Owned" : True,
+    "Colors" : ["Black", "White", "Silver"]
+}
+
+
