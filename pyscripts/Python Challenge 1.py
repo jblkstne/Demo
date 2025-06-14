d6db40dbc0e5c363710faf57eb5d96db8b99d898
james <jblkstne@gmail.com>
2025-06-14 18:10:39 -0400

    Continuing to work on the Python Challenge 1 script.

--- pyscripts/Python Challenge 1.py
+++ pyscripts/Python Challenge 1.py
@@ -33,3 +33,15 @@ else:
     print("This list is not empty")
     print(favorite_cars[0]["Make"]+" " + favorite_cars[0]["Model"] + " is my favorite car")
 
+code = 200
+match code:
+    case 404:
+        print("Page not found")
+    case 401:
+        print("Unauthorized access") 
+    case 403:
+        print("Forbidden access")
+    case 301:
+        print("Page moved permanently")
+    case 200:
+        print("Page found")
