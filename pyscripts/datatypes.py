b2b439d245e50127fa6dbc23a38ce26eb3104af2
james <jblkstne@gmail.com>
2025-06-15 11:22:09 -0400

    Following video

--- pyscripts/datatypes.py
+++ pyscripts/datatypes.py
@@ -1,1 +1,7 @@
-my_integer = 42
+from classes.my_classes import Car
+
+my_car = Car(make="Jeep", model="Wagoneer", year=2024, mileage=3000, color="Grey", condition="New")
+print(my_car.__dict__)
+
+my_car2 = Car(make="Ford", model="Bronco", year="2021", mileage="30000", color="Grey", condition="New")
+print(my_car2.__dict__)
