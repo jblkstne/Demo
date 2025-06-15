4e52cb3586b1b370d7e941e62e4eedb64fbaacc1
james <jblkstne@gmail.com>
2025-06-15 11:48:47 -0400

    Class demonstration

--- pyscripts/datatypes.py
+++ pyscripts/datatypes.py
@@ -1,12 +1,13 @@
 # from classes.my_classes import Car
 
-# my_car = Car(make="Jeep", model="Wagoneer", year=2024, mileage=3000, color="Grey", condition="New")
-# print(my_car.__dict__)
+
 
 # my_car2 = Car(make="Ford", model="Bronco", year="2021", mileage="30000", color="Grey", condition="New")
 # print(my_car2.__dict__)
 
-my_string = "  Hello World  "
-my_integer = 42
-print(my_string.strip().capitalize())
-print(my_integer)
+from classes.my_classes import Car
+my_car = Car(make="Jeep", model="Wagoneer", year=2024, mileage=3000, color="Grey", condition="New")
+my_car.start()
+my_car.accelerate(20)
+my_car.accelerate(30)
+my_car.stop()
