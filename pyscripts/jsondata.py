63d3ce9d541f86897f88eb179cd0cb45758067e5
james <jblkstne@gmail.com>
2025-06-16 12:42:31 -0400

    Serialization practice

---
+++ pyscripts/jsondata.py
@@ -0,0 +1,47 @@
+import json
+
+#with open("r1.json") as file:
+#    data = json.load(file)
+
+router_json = '''
+{
+    "router": {
+        "hostname": "R1",
+        "interfaces": [
+            {
+                "id": "0",
+                "enabled": "true",
+                "name": "GigabitEthernet0/0",
+                "ip": "192.168.1.254",
+                "mask": "255.255.255.0"
+            },
+            {
+                "id": "1",
+                "enabled": "true",
+                "name": "GigabitEthernet0/1",
+                "ip": "172.16.1.2",
+                "mask": "255.255.255.0"
+            }
+        ],
+        "routing": {
+            "routes": [
+                {
+                    "destination": "192.168.2.0",
+                    "mask": "255.255.255.0",
+                    "gateway": "192.168.1.253"
+                },
+                {
+                    "destination": "0.0.0.0",
+                    "mask": "0.0.0.0",
+                    "gateway": "201.1.113.54"
+                }
+            ]
+        }
+    }
+}
+'''
+
+data = json.loads(router_json)
+
+
+print(router_json)
